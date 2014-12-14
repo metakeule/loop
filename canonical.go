74f76b459c22c4c76637110ff91955751ff3d7ae
Marc Rene Arns <arns@stridor.net>
2014-12-14 20:43:16 +0100

    make version an int

--- canonical.go
+++ canonical.go
@@ -1,16 +1,15 @@
 // +build !dev
 
-package loop 	// import "gopkg.in/metakeule/loop.v1"
+package loop // import "gopkg.in/metakeule/loop.v1"
 
 import (
 	"github.com/metakeule/nodouble"
 )
 
 const (
-	VERSION = "1"
+	VERSION = 1
 )
 
 func init() {
 	nodouble.AddPackage("github.com/metakeule/loop", VERSION)
 }
-
