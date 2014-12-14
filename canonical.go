846a5c1270cd695df7d3fb324d73c25ab2be1e9c
Marc Rene Arns <arns@stridor.net>
2014-12-14 20:17:28 +0100

    add init function to prevent double loading

--- canonical.go
+++ canonical.go
@@ -1,3 +1,16 @@
 // +build !dev
 
-package loop // import "gopkg.in/metakeule/loop.v1"
+package loop 	// import "gopkg.in/metakeule/loop.v1"
+
+import (
+	"github.com/metakeule/nodouble"
+)
+
+const (
+	VERSION = "1"
+)
+
+func init() {
+	nodouble.AddPackage("github.com/metakeule/loop", VERSION)
+}
+
