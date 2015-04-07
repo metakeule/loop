dbb3938119af964bd9e51cdc708b25f8c22e124f
Marc Rene Arns <arns@stridor.net>
2015-04-07 13:09:39 +0200

    remove SpinEvery

--- spinner/spinner.go
+++ spinner/spinner.go
@@ -3,7 +3,6 @@ package spinner
 import (
 	"gopkg.in/metakeule/loop.v4"
 	"os"
-	"time"
 )
 
 type Spinner struct {
@@ -14,17 +13,14 @@ func New() *Spinner {
 	return &Spinner{loop.New([]byte("-\\|/"))}
 }
 
+func NewReverse() *Spinner {
+	return &Spinner{loop.New([]byte("/|\\-"))}
+}
+
 func (s *Spinner) Spin() (err error) {
 	b := make([]byte, 1)
 	s.l.Read(b)
 
 	_, err = os.Stdout.WriteString("\b" + string(b))
 	return
-}
-
-func (s *Spinner) SpinEvery(d time.Duration) {
-	for {
-		s.Spin()
-		time.Sleep(d)
-	}
 }
--- spinner/spinner_test.go
+++ spinner/spinner_test.go
@@ -39,3 +39,36 @@ func TestSpin(t *testing.T) {
 		t.Errorf("got: %#v; expected: %#v", out, expected)
 	}
 }
+
+func TestSpinReverse(t *testing.T) {
+
+	old := os.Stdout // keep backup of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	sp := NewReverse()
+
+	sp.Spin()
+	sp.Spin()
+	sp.Spin()
+	sp.Spin()
+	sp.Spin()
+
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old // restoring the real stdout
+	out := <-outC
+
+	expected := "\b/\b|\b\\\b-\b/"
+	if out != expected {
+		t.Errorf("got: %#v; expected: %#v", out, expected)
+	}
+}
